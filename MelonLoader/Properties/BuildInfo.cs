43db41f5c7b6efc772f8c66320444f1cb217d852
AC69x <147009438+AC69x@users.noreply.github.com>
2024-05-13 03:33:28 +0200

    Update BuildInfo.cs

--- MelonLoader/Properties/BuildInfo.cs
+++ MelonLoader/Properties/BuildInfo.cs
@@ -4,7 +4,7 @@
     {
         public const string Name = "MelonLoader";
         public const string Description = "MelonLoader";
-        public const string Author = "Lava Gang";
+        public const string Author = "Lava Gang & AC69x";
         public const string Company = "discord.gg/2Wn3N2P";
         public const string Version = "0.6.2";
     }
