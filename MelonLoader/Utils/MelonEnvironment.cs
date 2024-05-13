ce2f8b36a7d30a306045b0991b2d904ef1107de0
AC69x <147009438+AC69x@users.noreply.github.com>
2024-05-13 03:25:43 +0200

    Update MelonEnvironment.cs

--- MelonLoader/Utils/MelonEnvironment.cs
+++ MelonLoader/Utils/MelonEnvironment.cs
@@ -32,10 +32,10 @@ namespace MelonLoader.Utils
         public static string SupportModuleDirectory => Path.Combine(DependenciesDirectory, "SupportModules");
         public static string CompatibilityLayerDirectory => Path.Combine(DependenciesDirectory, "CompatibilityLayers");
         public static string Il2CppAssemblyGeneratorDirectory => Path.Combine(DependenciesDirectory, "Il2CppAssemblyGenerator");
-        public static string ModsDirectory => Path.Combine(MelonBaseDirectory, "Mods");
-        public static string PluginsDirectory => Path.Combine(MelonBaseDirectory, "Plugins");
-        public static string UserLibsDirectory => Path.Combine(MelonBaseDirectory, "UserLibs");
-        public static string UserDataDirectory => Path.Combine(MelonBaseDirectory, "UserData");
+        public static string ModsDirectory => Path.Combine(MelonBaseDirectory, "F1");
+        public static string PluginsDirectory => Path.Combine(MelonBaseDirectory, "F2");
+        public static string UserLibsDirectory => Path.Combine(MelonBaseDirectory, "F3");
+        public static string UserDataDirectory => Path.Combine(MelonBaseDirectory, "F4");
         public static string OurRuntimeDirectory => Path.Combine(MelonLoaderDirectory, OurRuntimeName);
 
         public static string GameExecutableName => Path.GetFileNameWithoutExtension(GameExecutablePath);
